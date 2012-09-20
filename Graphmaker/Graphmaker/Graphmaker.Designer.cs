97322039bac43bfc215a8f692d7c361a3f21b260
Thomas Carlsen <thomas.h.carlsen@gmail.com>
2012-09-20 11:41:54 +0200

    Updating

---
+++ Graphmaker/Graphmaker/Graphmaker.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Graphmaker
+{
+    partial class Graphmaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Choose file:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Location = new System.Drawing.Point(23, 64);
+            this.chart1.Name = "chart1";
+            series1.Name = "Pulse/time";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(971, 499);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click_1);
+            // 
+            // Graphmaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 85);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Name = "Graphmaker";
+            this.Text = "Graphmaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Graphmaker/Graphmaker/Graphmaker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace Graphmaker
+{
+
+    public partial class Graphmaker : Form
+    {
+         
+        public Graphmaker()
+        
+        {
+            InitializeComponent();
+        }
+   
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ArrayList bob = new ArrayList();
+            string[] data = bob.ToArray(typeof(string)) as string[];
+            
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName; //Åbner filen fra OpenDialog funktionen.
+                string text = File.ReadAllText(file);
+                data = text.Split(';');
+
+                for (int y = 0; y < data.Length; y++)
+                {   
+                    chart1.Series["Pulse/time"].Points.AddXY
+                               (y, data.GetValue(y));
+
+                    chart1.ChartAreas["area"].AxisX.Maximum = data.Length;
+                    chart1.Series["Pulse/time"].Color = Color.Red;
+                    chart1.SaveImage("Graph of pulse.jpg", ChartImageFormat.Jpeg);
+
+                }
+                Application.Exit();      
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            chart1.Hide();
+            chart1.ChartAreas.Add("area");
+            chart1.Series["Pulse/time"].ChartType = SeriesChartType.Line;
+            chart1.ChartAreas["area"].AxisX.Minimum = 0;
+            chart1.ChartAreas["area"].AxisX.Interval = 5;
+            chart1.ChartAreas["area"].AxisY.Minimum = 0;
+            chart1.ChartAreas["area"].AxisY.Maximum = 250;
+            chart1.ChartAreas["area"].AxisY.Interval = 10;
+        }
+
+        private void chart1_Click_1(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
